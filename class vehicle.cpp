6713697ae0c84dd035ca9b244f1e5ce90b966f66
jasnoorkaur15 <kjasnoor15@gmail.com>
2025-09-14 18:09:32 +0530

    Create class vehicle.cpp

---
+++ class vehicle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Base class
+class Vehical{
+    public:
+    string brand = "Ford";
+    void color(){
+        cout<<"Red!\n";
+    }
+};
+//Drived class
+class Car: public Vehical{
+    public:
+    string model = "Mustang";
+};
+int main(){
+    
+    Car mycar;
+    mycar.color();
+    cout<<mycar.brand +" "+mycar.model;
+    
+    return 0;
+}
+
+//OUTPUT
+//Red!
+//Ford Mustang
